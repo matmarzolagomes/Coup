14e04b4fe6179ccc737aaaecf4173151c5475d48
StormLotus <bloodrodrigo@gmail.com>
2017-05-25 14:06:38 -0300

    Player
    Signed-off-by: StormLotus <bloodrodrigo@gmail.com>

---
+++ src/ssc0103/coup/game/Deck.java
@@ -0,0 +1,12 @@
+package ssc0103.coup.game;
+
+import java.util.LinkedList;
+
+@SuppressWarnings("serial")
+public class Deck extends LinkedList<String> {
+	public Deck() {
+	}
+
+	public static void main(String[] args) {
+	}
+}
---
+++ src/ssc0103/coup/game/Player.java
@@ -0,0 +1,66 @@
+package ssc0103.coup.game;
+
+import java.util.Random;
+
+public class Player {
+	private Deck hand;
+	public int money;
+	
+	public Player() {
+		hand = new Deck();
+		money = 2;
+	}
+	
+	public boolean Income() {
+		money += 1;
+		return true;
+	}
+	
+	public boolean Foreign() {
+		money += 2;
+		return true;
+	}
+	
+	public boolean Coup() {
+		if(money < 7) return false;
+		money -= 7;
+		return true;
+	}
+	
+	public boolean Taxes() {
+		money += 3;
+		return true;
+	}
+	
+	public boolean Assassinate() {
+		if(money < 3) return false;
+		money -= 3;
+		return true;
+	}
+	
+	public boolean Steal(Player other) {
+		other.money -= 2;
+		if(other.money >= 0) this.money += 2;
+		else if(other.money == -1) this.money += 1;
+		return true;
+	}
+	
+	public boolean Draw(Deck board) {
+		Random randomizer = new Random();
+		hand.add(board.get(randomizer.nextInt(board.size())));
+		hand.add(board.get(randomizer.nextInt(board.size())));
+		return true;
+	}
+	
+	public boolean removeCard(String card) {
+		if(hand.remove(card)) return true;
+		return false;
+	}
+	
+	public boolean checkCard(String card) {
+		return hand.contains(card);
+	}
+
+	public static void main(String[] args) {
+	}
+}
