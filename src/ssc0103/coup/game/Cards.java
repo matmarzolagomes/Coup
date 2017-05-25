16f1888735377ac309ebf529bf4cc8f3bf0591e4
StormLotus <bloodrodrigo@gmail.com>
2017-05-25 12:49:40 -0300

    Packages class prototype
    Change-Id: I1079e0ed783c11f6528e3d70f05bd90e1b232dca
    Signed-off-by: StormLotus <bloodrodrigo@gmail.com>

---
+++ src/ssc0103/coup/game/Cards.java
@@ -0,0 +1,14 @@
+package ssc0103.coup.game;
+
+public abstract class Cards {
+
+	public Cards() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ssc0103/coup/main/Coup.java
@@ -0,0 +1,14 @@
+package ssc0103.coup.main;
+
+public class Coup {
+
+	public Coup() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
