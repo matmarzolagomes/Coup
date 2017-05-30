7724b8dfeb9e268c30241beec0ade6a8c05dc35d
9779433 <9779433@6-303-08.grad.usricmc.icmc.usp.br>
2017-05-30 09:54:02 -0300

    Add

--- src/scc0103/coup/lan/Client.java
+++ src/scc0103/coup/lan/Client.java
@@ -1,46 +1,40 @@
 package scc0103.coup.lan;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
-
 public class Client {
-	
+
 	public static void main(String[] args) {
-		 	
-	     Socket cliente = null;
+
+		Socket cliente = null;
 		try {
-			cliente = new Socket("192.168.56.1", 30000);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	     System.out.println("O cliente se conectou ao servidor!");
-	     
-	     Scanner teclado = new Scanner(System.in);
-	     PrintStream saida = null;
-		try {
+			cliente = new Socket("172.26.205.23", 8080);
+
+			System.out.println("O cliente se conectou ao servidor!");
+
+			Scanner teclado = new Scanner(System.in);
+			PrintStream saida = null;
 			saida = new PrintStream(cliente.getOutputStream());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	     
-	     while (teclado.hasNextLine()) {
-	       saida.println(teclado.nextLine());
-	     }
-	     
-	     saida.close();
-	     teclado.close();
-	     try {
+
+			while (teclado.hasNextLine()) {
+				saida.println(teclado.nextLine());
+			}
+
+			saida.close();
+			teclado.close();
+
 			cliente.close();
+
+		} catch (UnknownHostException e) {
+			e.getMessage();
+			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.getMessage();
 			e.printStackTrace();
 		}
-	   }
+	}
 }
