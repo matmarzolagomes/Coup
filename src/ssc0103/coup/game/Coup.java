559d24cbdaa1bf6524f08efd6469bd7255e08b96
Matheus <matmarzolagomes@gmail.com>
2017-05-31 18:22:58 -0300

    Lógica do jogo implementada

--- src/ssc0103/coup/game/Coup.java
+++ src/ssc0103/coup/game/Coup.java
@@ -32,13 +32,13 @@ public abstract class Coup {
 		if(players.get(index).getHand().size() == 0) players.remove(index);
 	}
 	
-	public boolean play(int action, int from, int to, boolean contest, boolean block) throws PException {
+	public boolean play(int action, int from, int to, boolean contest, boolean contest2, boolean block) throws PException {
 		boolean ret = false;
 		
 		switch (action) {
 		case(0):
 			// Income
-			ret = players.get(from).Income();
+			ret = players.get(from).income();
 		
 			break;
 		case(1):
@@ -47,20 +47,20 @@ public abstract class Coup {
 				// to == blocker
 				if(players.get(to).checkCard("Duke")) {
 					while(!ret)
-						ret = players.get(from).removeCard(getInput());
+						ret = players.get(from).removeCard(getInput(), dead);
 				} else { 
 					while(!ret)
-						ret = players.get(to).removeCard(getInput());
-					ret = players.get(from).Foreign();
+						ret = players.get(to).removeCard(getInput(), dead);
+					ret = players.get(from).foreign();
 				}
-			} else if(!block) ret = players.get(from).Foreign();
+			} else if(!block) ret = players.get(from).foreign();
 		
 			break;
 		case(2):
 			// Coup
-			if(players.get(from).Coup())
+			if(players.get(from).coup())
 				while(!ret)
-					ret = players.get(to).removeCard(getInput());
+					ret = players.get(to).removeCard(getInput(), dead);
 				
 			break;
 		case(3):
@@ -69,34 +69,92 @@ public abstract class Coup {
 				// to == blocker
 				if(players.get(from).checkCard("Duke")) {
 					while(!ret)
-						ret = players.get(to).removeCard(getInput());
+						ret = players.get(to).removeCard(getInput(), dead);
 				} else {
 					while(!ret)
-						ret = players.get(from).removeCard(getInput());
-					ret = players.get(from).Taxes();
+						ret = players.get(from).removeCard(getInput(), dead);
+					ret = players.get(from).taxes();
 				}
-			} else ret = players.get(from).Taxes();
+			} else ret = players.get(from).taxes();
 		
 			break;
 		case(4):
 			// Assassinate
-			if (contest) {
-				
-			} else ret = player.get(to).a
+			if (players.get(from).assassinate()) {
+				if (contest) {
+					// Se contestar
+					if (players.get(from).checkCard("Assassino")) {
+						while(!ret)ret = players.get(to).removeCard(getInput(), dead);
+						ret = false;
+						while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+						
+					} else
+						while (!ret) ret = players.get(from).removeCard(getInput(), dead);
+					
+				} else if (block) {
+					// se bloquear com a condessa
+					if (contest2) {
+						// se contestar que o cara tem a condessa
+						if (players.get(to).checkCard("Condessa")) {
+							while(!ret) ret = players.get(from).removeCard(getInput(), dead);
+							
+						} else {
+							while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+							ret = false;
+							while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+							
+						}
+						
+					} else ret = true;
+				} else 
+					while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+			}
 			
 			break;
 		case(5):
 			// Steal
+			if (contest) {
+				// se o cara que for roubado contestar o que tentou roubar
+				if (players.get(from).checkCard("Capitao")) {
+					while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+					ret = players.get(from).steal(players.get(to));
+				} else 
+					while(!ret) ret = players.get(from).removeCard(getInput(), dead);
+				
+			} else if (block){
+				// se o segundo cara tentar bloquear com o capitao ou embaixador
+				if (contest2) {
+					// se o primeiro contestar o bloqueio do segundo
+					if (players.get(to).checkCard("Embaixador") || players.get(to).checkCard("Capitao"))
+						while(!ret) ret = players.get(from).removeCard(getInput(), dead);
+					 else {
+						 while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+						 ret = players.get(from).steal(players.get(to));
+					 }
+				} else ret = true;
+					
+			} else ret = players.get(from).steal(players.get(to));
 			
 			break;
 		case(6):
 			// Swap
+			if (contest) {
+				if (players.get(from).checkCard("Embaixador")) {
+					while(!ret) ret = players.get(to).removeCard(getInput(), dead);
+				} else {
+					while(!ret) ret = players.get(from).removeCard(getInput(), dead);
+				}
+			} else {
+				players.get(from).draw(board);
+				while(!ret) ret = players.get(from).removeCard(getInput(), dead);
+				ret = false;
+				while(!ret) ret = players.get(from).removeCard(getInput(), dead);
+				
+			}
 			
 			break;
 		default:
 			throw new PException("Invalid play.");
-			
-			break;
 		
 		}
 		
