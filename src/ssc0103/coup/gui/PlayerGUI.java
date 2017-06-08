422d57702bae9f42b3cce96284ee0914f4a9f087
Matheus <matmarzolagomes@gmail.com>
2017-06-08 17:04:05 -0300

    Tabela de jogadores da interface implementada (falta algumas coisas)

--- src/ssc0103/coup/gui/PlayerGUI.java
+++ src/ssc0103/coup/gui/PlayerGUI.java
@@ -1,67 +1,68 @@
 package ssc0103.coup.gui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.util.LinkedList;
+import java.awt.BorderLayout;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import ssc0103.coup.game.Deck;
 import ssc0103.coup.game.Player;
 
 @SuppressWarnings("serial")
 public class PlayerGUI extends JPanel{
-	LinkedList<Player> players;
+	HashMap<String,Player> players;
+	JTable table;
+	String[] column;
+	Object[][] data;
+	JScrollPane scrollPane;
 	
-	public PlayerGUI(LinkedList<Player> players) {
+	
+	public PlayerGUI(HashMap<String,Player> players) {
 		super();
 		this.players = players;
-		this.setLayout(new GridBagLayout());
+		this.setLayout(new BorderLayout());
+		table = new JTable();
+		column = new String[] {"Jogadores", "Moedas", "Cartas"};
+		data = new Object[players.size()][3];
+		
+		createTable();
 	}
 	
-	public void showPlayers() {
-		GridBagConstraints c = new GridBagConstraints();
+	private void createTable() {
+		int i = 0;
 		
-		c.weighty = 0.1;
-		c.gridy = 0;
-		c.anchor = GridBagConstraints.CENTER;
-		
-		c.gridx = 0;
-		c.weightx = 0.8;
-		this.add(new JLabel("JOGADORES"), c);
-		c.gridx = 1;
-		c.weightx = 0.1;
-		this.add(new JLabel("MOEDAS"), c);
-		c.gridx = 2;
-		c.weightx = 0.1;
-		this.add(new JLabel("CARTAS"), c);
-		c.gridy++;
-		
-		for (Player player : players) {
-			c.gridx = 0;
-			c.weightx = 0.8;
-			this.add(new JLabel(player.getName()), c);
-			c.gridx = 1;
-			c.weightx = 0.1;
-			this.add(new JLabel(player.getMoney() + ""), c);
-			c.gridx = 2;
-			c.weightx = 0.1;
-			this.add(new JLabel(player.getHand().size() + ""), c);
-			c.gridy++;
+		for (Player player : players.values()) {
+			data[i][0] = player.getName();
+			data[i][1] = player.getMoney();
+			data[i][2] = player.getHand().size();
+			i++;
 		}
 		
-		if(players.size() <= 10) {
-			c.weighty = 1 - (players.size() * 0.1);
-			this.add(new JPanel(), c);
-		} else {
-			//this.setLayout(new GridLayout(12, 3));
-			JScrollPane scroll = new JScrollPane();
-			//scroll.setVisible(true);
-			this.add(scroll);
+		table = new JTable(data, column);
+		
+		scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+		
+		add(scrollPane);
+		
+	}
+	
+	public void attTable() {
+		int i = 0;
+		
+		for (Player player : players.values()) {
+			if((int)data[i][1] != player.getMoney()) {
+				data[i][1] = player.getMoney();
+			};
+			if((int)data[i][2] != player.getHand().size()) {
+				data[i][2] = player.getHand().size();
+			}
+			i++;
 		}
+		
 	}
 	
 	public static void main(String[] args) {
@@ -69,67 +70,70 @@ public class PlayerGUI extends JPanel{
 		game.setSize(300, 400);
 		game.setVisible(true);
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		LinkedList<Player> p = new LinkedList<Player>();
+		HashMap<String,Player> p = new HashMap<String,Player>();
 		Player p1;
 		
 		Deck deck = new Deck();
 		deck.createDeck(4);
 		
-		p1 = new Player("Matheus");
+		p1 = new Player("um");
 		p1.income();
 		p1.draw(deck);
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("Rodrigo");
+		p1 = new Player("dois");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("Ola");
+		p1 = new Player("tres");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("teste");
+		p1 = new Player("indiozinhos");
 		p1.income();
 		p1.draw(deck);
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("hey");
+		p1 = new Player("quatro");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("soul");
+		p1 = new Player("cinco");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("Matheus");
+		p1 = new Player("seis");
 		p1.income();
 		p1.draw(deck);
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
-		p1 = new Player("abc");
+		p1 = new Player("indiozinhos");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
 		p1 = new Player("aeeee");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
 		p1 = new Player("Matheus");
 		p1.income();
 		p1.draw(deck);
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
 		p1 = new Player("Rodrigo");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
 		p1 = new Player("Ola");
 		p1.foreign();
-		p.add(p1);
+		p.put(p1.getName(), p1);
 		
 		PlayerGUI list = new PlayerGUI(p);
-		list.showPlayers();
+		list.createTable();
+		list.attTable();
 		game.add(list);
+		
+		
 	}
 
 }
