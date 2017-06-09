13d8e7fa6f922685755b70352bc07906eb6d4a91
Bruno M. <bruno.mendes.costa@usp.br>
2017-06-09 16:33:28 -0300

    Modularização do Código.

--- src/ssc0103/coup/lan/Board.java
+++ src/ssc0103/coup/lan/Board.java
@@ -15,62 +15,111 @@ import javax.swing.JOptionPane;
 
 import ssc0103.coup.exception.PException;
 
+/**
+ * Classe Board, caracteriza-se por ser o servidor local do jogo, gerenciando as
+ * conexões com os jogadores da partida.
+ * 
+ * @author Bruno M.
+ *
+ */
 public class Board {
-	private static final int LIMITE = 100;
+	private static final int LIMIT = 100;
 	private int port;
 	private int numPlayers;
 	private ServerSocket board;
 	private Map<String, Socket> players;
 
-	public Board() throws IOException {
-		/* Obtém pota de conexão e verifica validade. */
-		while (true) {
-			try {
-				String msg = JOptionPane.showInputDialog("Informe a porta de conexão com o jogo:");
-				if (msg == null)
-					System.exit(0);
+	/**
+	 * Construtor da Classe Board, responsável por iniciliazar o servidor do
+	 * jogo.
+	 */
+	public Board() {
+		/* Ativa o servidor na rede. */
+		serverUp();
 
-				this.port = Integer.parseInt(msg);
-				board = new ServerSocket(this.port);
+		/* Obtém número de jogadores da partida. */
+		getNumPlayers();
 
-				System.out.println("Mesa Ativada.");
-				System.out.println("Porta " + this.port + " aberta!");
-				break;
-			} catch (IOException | IllegalArgumentException e) {
-				JOptionPane.showMessageDialog(null, "Porta de conexão inválida.", "Erro", JOptionPane.ERROR_MESSAGE);
-			}
-		}
+		/* Indica status do servidor em tempo real. */
+		serverStatus();
 
-		/* Obtém número de jogadores e verifica validade. */
-		while (true) {
-			try {
-				String msg = JOptionPane.showInputDialog(
-						"Informe o número de jogadores da partida:\nMínimo 2. \nMáximo " + LIMITE + ".");
-
-				if (msg == null) {
-					board.close();
-					System.exit(0);
-				}
-
-				this.numPlayers = Integer.parseInt(msg);
-				if (this.numPlayers < 2 || this.numPlayers > LIMITE)
-					throw new NumberFormatException();
-				break;
-			} catch (NumberFormatException e) {
-				JOptionPane.showMessageDialog(null, "O número de jogares informado é inválido.", "Erro",
-						JOptionPane.ERROR_MESSAGE);
-			}
-		}
-
+		/* Inicializa o HashMap de jogadores conectados. */
 		this.players = new HashMap<String, Socket>(this.numPlayers);
 	}
 
+	/**
+	 * Indica em tempo real as informações e o status do servidor.
+	 */
+	private void serverStatus() {
+		new Thread(() -> {
+			System.out.println("Mesa Ativada.");
+			System.out.println("Porta " + this.port + " aberta!");
+		}).start();
+	}
+
+	/**
+	 * Obtém o número de players da partida e verifica a sua validade.
+	 */
+	private void getNumPlayers() {
+		String msg = "Informe o número de jogadores da partida:\nMínimo 2.\nMáximo " + LIMIT + ".";
+		try {
+			msg = JOptionPane.showInputDialog(msg);
+
+			if (msg == null) {
+				board.close();
+				System.exit(0);
+			}
+
+			this.numPlayers = Integer.parseInt(msg);
+
+			if (this.numPlayers < 2 || this.numPlayers > LIMIT)
+				throw new IllegalArgumentException();
+
+		} catch (IllegalArgumentException e) {
+			msg = "O número de jogares informado é inválido.";
+			JOptionPane.showMessageDialog(null, msg, "Erro", JOptionPane.ERROR_MESSAGE);
+			getNumPlayers();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Obtém a porta de conexão com o servidor e verifica a sua validade.
+	 */
+	private void serverUp() {
+		String msg = "Informe a porta de conexão com o jogo:";
+
+		try {
+			msg = JOptionPane.showInputDialog(msg);
+
+			if (msg == null)
+				System.exit(0);
+
+			this.port = Integer.parseInt(msg);
+			board = new ServerSocket(this.port);
+
+		} catch (IOException | IllegalArgumentException e) {
+			msg = "Porta de conexão inválida.";
+			JOptionPane.showMessageDialog(null, msg, "Erro", JOptionPane.ERROR_MESSAGE);
+			serverUp();
+		}
+	}
+
+	/**
+	 * Método responsável por gerenciar as conexões do servidor e da partida.
+	 * 
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws PException
+	 */
 	public void execute() throws IOException, ClassNotFoundException, PException {
 		Actions actions;
 		ObjectInputStream input;
 		ObjectOutputStream output;
 		String playerName;
-		List<String> log = new ArrayList<>();		
+		List<String> log = new ArrayList<>();
 
 		try {
 			/* Aguarda até que todos os players tenham se conectado. */
@@ -109,11 +158,16 @@ public class Board {
 									msg = "O nome do jogador é muito extenso." + "\nTamanho Máximo = 16 caracteres.";
 								} else {
 									msg = "Aguadando demais jogadores.";
-									/* Insere na lista de conexões de players. */
+									/*
+									 * Insere na lista de conexões de players.
+									 */
 									players.put(act.getFrom(), player);
 									System.out.println("Jogador " + act.getFrom() + " se juntou a mesa.");
 
-									/* Envia mensagem de aguardando demais jogadores. */									
+									/*
+									 * Envia mensagem de aguardando demais
+									 * jogadores.
+									 */
 									act = new Actions();
 									act.setId(Actions.SERVER_MESSAGE);
 									act.setMessage(msg);
@@ -122,7 +176,7 @@ public class Board {
 									break;
 								}
 
-								/* Envia uma notificação ao jogador. */								
+								/* Envia uma notificação ao jogador. */
 								act = new Actions();
 								act.setId(Actions.SERVER_MESSAGE);
 								act.setMessage(msg);
@@ -140,17 +194,18 @@ public class Board {
 					}
 				}).start();
 			}
-			
-			System.out.println(Thread.activeCount());			
+
+			System.out.println(Thread.activeCount());
 
 			/* Aguarda até que todas as Threads tenham sido finalizadas. */
-			while (Thread.activeCount() > 1);
-			
+			while (Thread.activeCount() > 1)
+				;
+
 			System.out.println(Thread.activeCount());
-						
+
 			/* Inicializa a mecânica do jogo. */
-			CoupLan coup = new CoupLan(this.numPlayers, players.keySet().toArray(new String[players.size()]));			
-												
+			CoupLan coup = new CoupLan(this.numPlayers, players.keySet().toArray(new String[players.size()]));
+
 			/* Notifica todos os players para iniciar o jogo. */
 			for (Socket player : players.values()) {
 				new Thread(() -> {
@@ -159,28 +214,29 @@ public class Board {
 						Actions action = new Actions();
 						action.setCoup(coup);
 						action.setLog("Início do Jogo.");
-						log.add(action.getLog());												
-						action.setId(Actions.LOAD_INTERFACE);						
+						log.add(action.getLog());
+						action.setId(Actions.LOAD_INTERFACE);
 						oos.writeObject(action);
 						oos.flush();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}).start();
-			}						
-						
+			}
+
 			/* Aguarda até que todas as Threads tenham sido finalizadas. */
-			while (Thread.activeCount() > 1);
+			while (Thread.activeCount() > 1)
+				;
 
 			/* Roda o Jogo até que reste apenas 1 player. */
-			for (Iterator<String> iterator = players.keySet().iterator(); players.size() > 1; 
-					iterator = !iterator.hasNext() ? players.keySet().iterator() : iterator) {
+			for (Iterator<String> iterator = players.keySet().iterator(); players
+					.size() > 1; iterator = !iterator.hasNext() ? players.keySet().iterator() : iterator) {
 
 				/* Obtém a conexão do player do turno. */
 				playerName = (String) iterator.next();
 				Socket player = players.get(playerName);
 				System.out.println("Turno do Jogador " + playerName + ".");
-				
+
 				/* Envia ao player as suas ações. */
 				output = new ObjectOutputStream(player.getOutputStream());
 				actions = new Actions();
@@ -189,55 +245,58 @@ public class Board {
 				actions.setFrom(playerName);
 				output.writeObject(actions);
 				output.flush();
-				
+
 				/* Recebe a resposta do jogador. */
-				input = new ObjectInputStream(player.getInputStream());				
+				input = new ObjectInputStream(player.getInputStream());
 				actions = (Actions) input.readObject();
-												
-				if(actions.getId() == Actions.INCOME) {
-					coup.play(Actions.INCOME, actions.getFrom(), actions.getTo(), actions.isContest(), actions.isBlock());
-					
+
+				if (actions.getId() == Actions.INCOME) {
+					coup.play(Actions.INCOME, actions.getFrom(), actions.getTo(), actions.isContest(),
+							actions.isBlock());
+
 					/* Atualiza o jogo de todos os players. */
 					String msg = "O jogador " + actions.getFrom() + " recebeu 1 moeda.";
 					actions.setId(Actions.UPDATE_ALL_INTERFACE);
 					actions.setCoup(coup);
 					actions.setLog(msg);
 					log.add(msg);
-					
+
 					/* Envia a ação para todos os players. */
-					for (Socket p : players.values()) {						
+					for (Socket p : players.values()) {
 						output = new ObjectOutputStream(p.getOutputStream());
 						output.writeObject(actions);
 						output.flush();
-					}														
-				} else if(actions.getId() == Actions.FOREIGN) {
-					//coup.play(Actions.FOREIGN, actions.getFrom(), actions.getTo(), contest, block)
-					
-				} else if(actions.getId() == Actions.COUP) {
-					coup.play(Actions.COUP, actions.getFrom(), actions.getTo(), actions.isContest(), actions.isBlock());					
+					}
+				} else if (actions.getId() == Actions.FOREIGN) {
+					// coup.play(Actions.FOREIGN, actions.getFrom(),
+					// actions.getTo(), contest, block)
+
+				} else if (actions.getId() == Actions.COUP) {
+					coup.play(Actions.COUP, actions.getFrom(), actions.getTo(), actions.isContest(), actions.isBlock());
 					/* REMOVER DO ITERATOR O PLAYER QUE LEVOU O GOLPE. */
-					
+
 					/* Atualiza o jogo de todos os players. */
-					String msg = "O jogador " + actions.getFrom() + " deu um golpe de estado no jogador " + actions.getTo() + ".";
+					String msg = "O jogador " + actions.getFrom() + " deu um golpe de estado no jogador "
+							+ actions.getTo() + ".";
 					actions.setId(Actions.UPDATE_ALL_INTERFACE);
 					actions.setCoup(coup);
 					actions.setLog(msg);
 					log.add(msg);
-					
+
 					/* Envia a ação para todos os players. */
-					for (Socket p : players.values()) {						
+					for (Socket p : players.values()) {
 						output = new ObjectOutputStream(p.getOutputStream());
 						output.writeObject(actions);
 						output.flush();
-					}							
-				} else if(actions.getId() == Actions.TAXES) {
-					
-				} else if(actions.getId() == Actions.ASSASSINATE) {
-					
-				} else if(actions.getId() == Actions.STEAL) {
-					
-				} else if(actions.getId() == Actions.SWAP) {
-					
+					}
+				} else if (actions.getId() == Actions.TAXES) {
+
+				} else if (actions.getId() == Actions.ASSASSINATE) {
+
+				} else if (actions.getId() == Actions.STEAL) {
+
+				} else if (actions.getId() == Actions.SWAP) {
+
 				}
 				// Método para remover.
 				// iterator.remove();
@@ -272,7 +331,8 @@ public class Board {
 			// }
 
 			/* Mantem o Servidor Rodando. */
-			while (true);
+			while (true)
+				;
 
 		} catch (IOException e) {
 			e.printStackTrace();
