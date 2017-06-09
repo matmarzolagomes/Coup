02e93eddd91ea6f92e7b153d16b91f3116e87113
victorhenrique97 <victorhrs97@gmail.com>
2017-06-09 20:52:55 -0300

    Pop Ups do jogo

---
+++ src/ssc0103/coup/lan/SwingJRadioButtonDemo.java
@@ -0,0 +1,139 @@
+package ssc0103.coup.lan;
+
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.SwingUtilities;
+
+public class SwingJRadioButtonDemo extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+
+	private JButton buttonOK = new JButton("OK");
+	private JRadioButton renda = new JRadioButton("Renda");
+	private JRadioButton ajuda = new JRadioButton("Ajuda Externa");
+	private JRadioButton taxa = new JRadioButton("Obter Taxas");
+	private JRadioButton assassinar = new JRadioButton("Assassinar Jogador");
+	private JRadioButton extorquir = new JRadioButton("Extorquir Jogador");
+	private JRadioButton trocar = new JRadioButton("Trocar Cartas");
+	private JRadioButton golpe = new JRadioButton("Golpe de Estado");
+
+	private JLabel labelImage = new JLabel();
+
+	private ImageIcon rendai = new ImageIcon("images2/moeda.png");
+	private ImageIcon ajudai = new ImageIcon("images2/moeda1.png");
+	private ImageIcon taxai = new ImageIcon("images2/moedas.png");
+	private ImageIcon assassinari = new ImageIcon("images2/foice.png");
+	private ImageIcon extorquiri = new ImageIcon("images2/roubo.png");
+	private ImageIcon trocari = new ImageIcon("images2/troca.png");
+	private ImageIcon golpei = new ImageIcon("images2/golpe.png");
+	
+	public SwingJRadioButtonDemo() {
+		
+		super("Ações");
+		setSize(400, 600);
+		setResizable(false);
+		this.setLocationRelativeTo(null);
+		
+		golpei.setImage(golpei.getImage().getScaledInstance(75, 75, 100));
+		rendai.setImage(rendai.getImage().getScaledInstance(80, 65, 100));
+		taxai.setImage(taxai.getImage().getScaledInstance(75, 75, 100));
+		assassinari.setImage(assassinari.getImage().getScaledInstance(75, 75, 100));
+		extorquiri.setImage(extorquiri.getImage().getScaledInstance(75, 75, 100));
+		trocari.setImage(trocari.getImage().getScaledInstance(75, 75, 100));
+		ajudai.setImage(ajudai.getImage().getScaledInstance(75, 75, 100));
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(renda);
+		group.add(ajuda);
+		group.add(taxa);
+		group.add(assassinar);
+		group.add(extorquir);
+		group.add(trocar);
+		group.add(golpe);
+
+		setLayout(new GridBagLayout());
+
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.anchor = GridBagConstraints.FIRST_LINE_START;	//alinhamento
+		constraints.insets = new Insets(10, 10, 10, 10);			//espaco entre celulas
+	
+		add(renda, constraints);
+		add(ajuda, constraints);
+		add(taxa, constraints);
+		add(assassinar, constraints);
+		add(extorquir, constraints);
+		add(trocar, constraints);
+		add(golpe, constraints);
+		
+		constraints.anchor = GridBagConstraints.CENTER;
+		add(labelImage, constraints);
+		buttonOK.setText("Confirmar");
+		add(buttonOK, constraints);
+
+		RadioButtonActionListener actionListener = new RadioButtonActionListener();
+		ajuda.addActionListener(actionListener);
+		taxa.addActionListener(actionListener);
+		golpe.addActionListener(actionListener);
+		renda.addActionListener(actionListener);
+		assassinar.addActionListener(actionListener);
+		trocar.addActionListener(actionListener);
+		extorquir.addActionListener(actionListener);
+		
+		buttonOK.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				/*
+				if(ajuda.isSelected()) 
+				else if(taxa.isSelected())
+				else if(golpe.isSelected())
+				else if(renda.isSelected())
+				else if(assassinar.isSelected())
+				else if(trocar.isSelected())
+				else if(extorquir.isSelected())*/
+			}
+		});
+		
+	}
+
+	class RadioButtonActionListener implements ActionListener {
+		
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			
+			JRadioButton button = (JRadioButton) event.getSource();
+			
+			if (button == ajuda) labelImage.setIcon(ajudai);
+			else if (button == taxa) labelImage.setIcon(taxai);
+			else if (button == golpe) labelImage.setIcon(golpei);
+			else if (button == renda) labelImage.setIcon(rendai);
+			else if (button == extorquir) labelImage.setIcon(extorquiri);
+			else if (button == trocar) labelImage.setIcon(trocari);
+			else if (button == assassinar) labelImage.setIcon(assassinari);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new SwingJRadioButtonDemo().setVisible(true);
+			}
+		});
+	}
+}
--- src/ssc0103/coup/lan/Teste.java
+++ src/ssc0103/coup/lan/Teste.java
@@ -6,11 +6,12 @@ import javax.swing.JOptionPane;
 public class Teste {
 	
 	int ret = -1;
-	ImageIcon duque, assassino, embaixador, capitao, condessa, coins, golpe;
+	ImageIcon duque, assassino, embaixador, capitao, condessa, coins, golpe, coup, icon;
 	String opcoes1[] = new String[] {"Aceitar", "Bloquear", "Contestar"};
 	String opcoes2[] = new String[] {"Permitir", "Negar"};
 	String opcoes3[] = new String[] {"Aceitar", "Contestar"};
 	String opcoes4[] = new String[] {"Aceitar", "Bloquear (Embaixador)", "Bloquear (Capitão)", "Contestar"};
+	String jogador = null;
 	
 	public Teste(){
 		
@@ -20,15 +21,19 @@ public class Teste {
 		embaixador = new ImageIcon("images/Embaixador.jpeg");
 		capitao = new ImageIcon("images/Capitao.jpeg");
 		condessa = new ImageIcon("images/Condessa.jpeg");
-		golpe = new ImageIcon("images/death.png");
+		golpe = new ImageIcon("images/golpe.png");
+		coup = new ImageIcon("images/coup_logo.png");
+		icon = new ImageIcon("images/icon.png");
 		
-		golpe.setImage(golpe.getImage().getScaledInstance(188, 172, 100));
+		golpe.setImage(golpe.getImage().getScaledInstance(150, 180, 100));
 		coins.setImage(coins.getImage().getScaledInstance(128, 128, 100));
 		duque.setImage(duque.getImage().getScaledInstance(150, 250, 100));
 		assassino.setImage(assassino.getImage().getScaledInstance(150, 250, 100));
 		embaixador.setImage(embaixador.getImage().getScaledInstance(150, 250, 100));
 		capitao.setImage(capitao.getImage().getScaledInstance(150, 250, 100));
 		condessa.setImage(condessa.getImage().getScaledInstance(150, 250, 100));
+		coup.setImage(coup.getImage().getScaledInstance(400, 100, 100));
+		icon.setImage(icon.getImage().getScaledInstance(128, 128, 100));
 		
 	}
 	public void popUpAcao(){
@@ -112,17 +117,44 @@ public class Teste {
 		JOptionPane.showOptionDialog(null, nome + " aplicou um golpe de estado em você!", "Golpe de Estado", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, golpe, null, null);
 	}
 	
+	public int popUpOpcoes(String[] acoes, String nome){
+		ret = JOptionPane.showOptionDialog(null, nome + " escolha a sua ação", "Sua Vez", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, coup, acoes, acoes[0]);
+		while(ret < 0){
+			popUpAcao();
+			ret = JOptionPane.showOptionDialog(null, nome + " escolha a sua ação", "Sua Vez", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, coup, acoes, acoes[0]);
+		}
+		return ret;
+	}
+	
+	public String popUpJogadores(String[] jogadores){
+
+		jogador = (String) JOptionPane.showInputDialog(null, "Escolha um Jogador\n \n", "Jogador", JOptionPane.INFORMATION_MESSAGE, icon, jogadores, jogadores[0]);
+		
+		while(jogador == null){
+			popUpAcao();
+			jogador = (String) JOptionPane.showInputDialog(null, "Escolha um Jogador\n \n", "Jogador", JOptionPane.INFORMATION_MESSAGE, icon, jogadores, jogadores[0]);
+		}
+		
+	return jogador;
+	
+	}
+	
 	
 	public static void main(String[] args){
 		
+		String a[] = new String[]{"Victor", "Bruno", "Matheus", "Rodrigo"};
+		String b[] = new String[]{"Renda", "Taxas", "Assassinar", "Extorquir", "Ajuda", "Trocar", "Golpe"};
 		Teste t = new Teste();
-		//System.out.println(t.popUpAssassino("Victor"));
-		//System.out.println(t.popUpTaxas("Victor"));
-		//System.out.println(t.popUpCondessa("Victor"));
-		//System.out.println(t.popUpTroca("Victor"));
-		//System.out.println(t.popUpExtorcao("Victor"));
-		//System.out.println(t.popUpBloqueioExtorcao("victor", "Capitao"));
-		//System.out.println(t.popUpBloqueioExtorcao("victor", "Embaixador"));
-		//t.popUpGolpe("victor");
+		System.out.println(t.popUpAssassino("Victor"));
+		System.out.println(t.popUpTaxas("Victor"));
+		System.out.println(t.popUpCondessa("Victor"));
+		System.out.println(t.popUpTroca("Victor"));
+		System.out.println(t.popUpExtorcao("Victor"));
+		System.out.println(t.popUpAjudaExterna("Victor"));
+		System.out.println(t.popUpBloqueioExtorcao("victor", "Capitao"));
+		System.out.println(t.popUpBloqueioExtorcao("victor", "Embaixador"));
+		t.popUpGolpe("Victor");
+		System.out.println(t.popUpOpcoes(b, "Victor"));
+		System.out.println(t.popUpJogadores(a));
 	}
 }
