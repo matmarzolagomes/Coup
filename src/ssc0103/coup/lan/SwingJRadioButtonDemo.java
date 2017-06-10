b17f7c190047b900b032bb457c52fdfb5f998234
Bruno M. <bruno.mendes.costa@usp.br>
2017-06-10 16:08:41 -0300

    Warning

--- src/ssc0103/coup/lan/SwingJRadioButtonDemo.java
+++ src/ssc0103/coup/lan/SwingJRadioButtonDemo.java
@@ -12,7 +12,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.SwingUtilities;
 
