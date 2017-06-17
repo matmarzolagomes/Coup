d4cbb9295e75b3f6d053c9bcf883f157cb2048e2
victorhenrique97 <victorhenrique97@NautilusII>
2017-06-17 00:08:35 -0300

    Tabela arrumada

--- src/ssc0103/coup/gui/PlayerGUI.java
+++ src/ssc0103/coup/gui/PlayerGUI.java
@@ -15,24 +15,27 @@ import javax.swing.table.DefaultTableCellRenderer;
 import ssc0103.coup.game.Player;
 
 @SuppressWarnings("serial")
-public class PlayerGUI extends JPanel{
+public class PlayerGUI extends JPanel {
 	JTable table;
 	String[] column;
 	Object[][] data;
 	JScrollPane scrollPane;
-	
+	int tablesize;
+
 	public PlayerGUI(HashMap<String, Player> players) {
 		super();
 		setLayout(new GridLayout(1, 1));
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
-		
-		column = new String[] {"Jogadores", "Moedas", "Cartas"};
+		tablesize = players.size();
+
+		column = new String[] { "Jogadores", "Moedas", "Cartas" };
 		data = new Object[players.size()][3];
 		createTable(players);
 	}
-	
-	private void createTable(HashMap<String,Player> players) {
+
+	private void createTable(HashMap<String, Player> players) {
 		int i = 0;
+
 		
 		for (Player player : players.values()) {
 			data[i][0] = player.getName();
@@ -40,42 +43,48 @@ public class PlayerGUI extends JPanel{
 			data[i][2] = player.getHand().size();
 			i++;
 		}
-		
+
 		table = new JTable(data, column);
 		table.setEnabled(false);
 		table.setSelectionBackground(Color.LIGHT_GRAY);
-		
+
 		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
 		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
 		table.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
 		table.getColumnModel().getColumn(2).setCellRenderer(centerRenderer);
-		
+
 		scrollPane = new JScrollPane(table);
 		table.setPreferredScrollableViewportSize(table.getPreferredSize());
 		table.setFillsViewportHeight(false);
 		table.setFont(new Font("Serif", Font.BOLD, table.getFont().getSize()));
-		
+
 		add(scrollPane);
 	}
-	
-	public void attTable(HashMap<String,Player> players) {
-		int i = 0;
-		
+
+	public void attTable(HashMap<String, Player> players) {
+
 		for (Player player : players.values()) {
-			if((int)data[i][1] != player.getMoney()) {
-				data[i][1] = player.getMoney();
-			}
-			if((int)data[i][2] != player.getHand().size()) {
-				data[i][2] = player.getHand().size();
-			}
 			
-			if ((int)data[i][2] == 0) 
-				table.addRowSelectionInterval(i, i);
-			
-			i++;
+			for (int j = 0; j<tablesize; ++j) {
+				
+				if (player.getName().equals(data[j][0])){
+					
+					if ((int) data[j][1] != player.getMoney()) 
+						data[j][1] = player.getMoney();
+					
+					if ((int) data[j][2] != player.getHand().size()) 
+					data[j][2] = player.getHand().size();
+				
+
+					if ((int) data[j][2] == 0)
+					table.addRowSelectionInterval(j, j);
+				}
+			}
 		}
+		
+		table.updateUI();
 	}
-	
+
 	public static void main(String[] args) {
 	}
 }
