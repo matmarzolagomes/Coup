18881ffa4168ac450220c5b2a9507a2b405e9580
Matheus <matmarzolagomes@gmail.com>
2017-06-17 00:31:09 -0300

    log fica no final

--- src/ssc0103/coup/gui/LogGUI.java
+++ src/ssc0103/coup/gui/LogGUI.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -34,9 +35,28 @@ public class LogGUI extends JPanel{
     
     public void insertLog(String string) {
         area.append(string+"\n");
+        area.setCaretPosition(area.getText().length());
+        
     }
     
     public static void main(String[] args) {
+    	LogGUI log = new LogGUI();
+    	JFrame frame = new JFrame("Game");
+    	frame.setVisible(true);
+    	log.insertLog("1");
+    	log.insertLog("2");
+    	log.insertLog("3");
+    	log.insertLog("4");
+    	log.insertLog("5");
+    	log.insertLog("6");
+    	log.insertLog("7");
+    	log.insertLog("8");
+    	log.insertLog("9");
+    	
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.add(log);
+        frame.pack();
     }
     
 }
