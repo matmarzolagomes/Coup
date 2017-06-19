43f9e899d2c148cddbebf94d3e4a017f063f4581
victorhenrique97 <victorhrs97@gmail.com>
2017-06-19 16:47:25 -0300

    Javadoc Exception.

--- src/ssc0103/coup/exception/LANExcpetion.java
+++ src/ssc0103/coup/exception/LANExcpetion.java
@@ -1,6 +1,14 @@
 package ssc0103.coup.exception;
 
+/**
+ * Exception usada para a parte de rede.
+ * 
+ * @author Bruno Mendes da Costa - Nº USP 9779433
+ *
+ */
 @SuppressWarnings("serial")
 public class LANExcpetion extends Exception {
-	public LANExcpetion(String message) { super(message); }
+	public LANExcpetion(String message) {
+		super(message);
+	}
 }
