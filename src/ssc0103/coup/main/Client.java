417935e74c1003e178b73c388afb976984d4083c
StormLotus <bloodrodrigo@gmail.com>
2017-06-19 14:41:03 -0300

    05

--- src/ssc0103/coup/main/Client.java
+++ src/ssc0103/coup/main/Client.java
@@ -2,6 +2,11 @@ package ssc0103.coup.main;
 
 import ssc0103.coup.lan.Player;
 
+/**
+ * Main do cliente.
+ * @author Rodrigo Geurgas Zavarizz 9791080
+ *
+ */
 public class Client {
 	public static void main(String[] args) {
 		new Player().execute();
--- src/ssc0103/coup/main/Server.java
+++ src/ssc0103/coup/main/Server.java
@@ -2,6 +2,11 @@ package ssc0103.coup.main;
 
 import ssc0103.coup.lan.Board;
 
+/**
+ * Main do servidor.
+ * @author Rodrigo Geurgas Zavarizz 9791080
+ *
+ */
 public class Server {
 	public static void main(String[] args) {
 		new Board().execute();
